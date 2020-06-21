df288ebc1cfaf86fc1071a11c1c56c75d0520563
LKhyeon <lkdh97@gmail.com>
2020-06-21 15:23:35 -0400

    hashrouter works. Changing style to meet the changes.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@
 import React from 'react';
 import {HashRouter, Switch, Route} from 'react-router-dom';
 
+// Import Styles
+import '../css/baseStyle.css';
+
 // Import other pages
 // import RedirectComponent from './components/Redirect';
 import MainPage from './components/MainPage';
